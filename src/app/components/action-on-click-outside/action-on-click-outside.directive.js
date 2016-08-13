a2e0b45c5af1aa29d9fe27fcf36f44feca93034a
Sergey Smirnov <s@tigr.bz>
2016-08-13 21:21:49 +0300

    clean timeout from sidebar directive.

--- src/app/components/action-on-click-outside/action-on-click-outside.directive.js
+++ src/app/components/action-on-click-outside/action-on-click-outside.directive.js
@@ -1,20 +1,19 @@
 angular.module('actionOnClickOutside', [])
-    .directive('actionOnClickOutside', function ($document, $timeout) {
+    .directive('actionOnClickOutside', function ($document) {
         return {
             restrict: 'A',
             link: function (scope, el, attrs) {
                 function listener(e) {
-                    if (e.target !== el) {
+                    if (e.target !== el[0]) {
                         scope.$eval(attrs.actionOnClickOutside);
                         scope.$applyAsync();
                     }
                 }
-                $timeout(function() {
-                    $document[0].addEventListener('click', listener);
-                    scope.$on('$destroy', function() {
-                        $document[0].removeEventListener('click', listener)
-                    })
-                });
+
+                $document[0].addEventListener('click', listener, true);
+                el.$on('$destroy', function() {
+                    $document[0].removeEventListener('click', listener, true)
+                })
             }
         }
     });
