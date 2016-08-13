92db12554dc176b4c27d280c545eefbebb798dd1
Sergey Smirnov <s@tigr.bz>
2016-08-13 21:33:38 +0300

    prevent collapse sidebar on click internal el.

--- src/app/components/action-on-click-outside/action-on-click-outside.directive.js
+++ src/app/components/action-on-click-outside/action-on-click-outside.directive.js
@@ -4,14 +4,19 @@ angular.module('actionOnClickOutside', [])
             restrict: 'A',
             link: function (scope, el, attrs) {
                 function listener(e) {
-                    if (e.target !== el[0]) {
-                        scope.$eval(attrs.actionOnClickOutside);
-                        scope.$applyAsync();
+                    var curElement = e.target;
+                    while (curElement) {
+                        if (curElement === el[0]) {
+                            return;
+                        }
+                        curElement = curElement.parentNode
                     }
+                    scope.$eval(attrs.actionOnClickOutside);
+                    scope.$applyAsync();
                 }
 
                 $document[0].addEventListener('click', listener, true);
-                el.$on('$destroy', function() {
+                el.on('$destroy', function() {
                     $document[0].removeEventListener('click', listener, true)
                 })
             }
